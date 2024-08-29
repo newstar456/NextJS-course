563347828f8ade563eddfe437733eaa49f239da9
Viktoria Vonsovych <vikvons@gmail.com>
2024-08-29 22:28:59 +0200

    changed structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
-import styles from "./page.module.css";
-import course from './course.json'
+import styles from './page.module.css'
 
-export default function Home() {
+
+export default async function Home() {
+
   
   return (
-    <main className={styles.main}>
-      <ul>
-        {course.lessons.map((lesson) => {
-          return <li key={lesson.name}>
-            <h2>{lesson.title}</h2>
-            <p>{lesson.points}</p>
+    <main>
 
-          </li>
-        })}
-      </ul>
     </main>
   );
 }
